ca9445c200177cba1ec57392420338fc0cf71058
Carina <carinausbeck@live.dk>
2020-10-16 14:27:39 +0200

    sec

---
+++ executequiz.js
@@ -0,0 +1,20 @@
+let myQuestions = [
+    {
+        question: "Hello?",
+        answers: {
+            a: 'cat',
+            b: 'world',
+            c: 'pizza'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "Are cats cute?",
+        answers: {
+            a: 'yes',
+            b: 'no',
+            c: 'maybe'
+        },
+        correctAnswer: 'a'
+    }
+];
